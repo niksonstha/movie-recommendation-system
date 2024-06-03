702b0d53493825153200507ccd38e059720bd03f
niksonstha <sthanikson10@gmail.com>
2024-06-03 10:33:01 +0545

    deleted unwanted files and folders and add bcryptjs package

--- backend/src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { createUser, loginUser } from "../controllers/userController.js";
-
-router.post("/createUser", createUser);
-router.get("/loginUser", loginUser);
-
-export default router;
